93c9e9cb07709a36f57204598bd72ab5e03cb7ca
Alessandro Molina <alessandro.molina@axant.it>
2015-11-06 15:59:59 +0100

    Add WebAssets support

---
+++ webassets/babelfilter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+from webassets.filter import Filter
+
+import dukpy
+
+
+__all__ = ('BabelJS', )
+
+
+class BabelJS(Filter):
+    name = 'babeljs'
+    max_debug_level = None
+
+    def setup(self):
+        super(BabelJS, self).setup()
+
+    def input(self, _in, out, **kw):
+        src = dukpy.babel_compile(_in.read())
+        out.write(src)
