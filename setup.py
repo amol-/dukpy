25bb4f604f65bad381a2df3488486af625151c70
Alessandro Molina <alessandro.molina@axant.it>
2015-11-22 19:25:24 +0100

    Add missing info to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import os
 from distutils.core import setup, Extension
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(HERE, 'README.rst')).read()
+except IOError:
+    README = ''
 
 duktape = Extension('dukpy._dukpy',
                     define_macros=[('DUK_OPT_DEEP_C_STACK', '1')],
@@ -12,9 +17,25 @@ setup(
     name='dukpy',
     version='0.0.1',
     description='Simple JavaScript interpreter for Python',
+    long_description=README,
+    keywords='javascript compiler babeljs coffeescript',
+    author='Alessandro Molina',
+    author_email='alessandro.molina@axant.it',
+    url='https://github.com/amol-/dukpy',
+    license='MIT',
     packages=['dukpy'],
     ext_modules=[duktape],
     package_data={
         'dukpy': ['*.js'],
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: JavaScript',
+    ]
 )
