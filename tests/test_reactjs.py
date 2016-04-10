ebf52caf6ee09ef1f15cb88815a1fb8008899c79
Alessandro Molina <alessandro.molina@axant.it>
2016-04-10 09:15:06 +0200

    Add tests for a React Component

--- tests/test_reactjs.py
+++ tests/test_reactjs.py
@@ -14,4 +14,35 @@ class TestReactJS(object):
             jsx,
             'ReactDOM.renderToStaticMarkup(react_hello, null);'
         ])
-        assert result == '<h1>Hello, world!</h1>'
+        assert result == '<h1>Hello, world!</h1>', res
+
+    def test_jsx_mixed(self):
+        code = '''
+var React = require('react/react'),
+ ReactDOM = require('react/react-dom-server');
+ReactDOM.renderToStaticMarkup(<h1>Hello, world!</h1>, null);
+'''
+        jsx = dukpy.jsx_compile(code)
+        res = dukpy.evaljs(jsx)
+        assert res == '<h1>Hello, world!</h1>', res
+
+    def test_react_binding(self):
+        code = '''
+var React = require('react/react'),
+ ReactDOM = require('react/react-dom-server');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <div className="helloworld">
+        Hello {this.props.data.name}
+      </div>
+    );
+  }
+});
+
+ReactDOM.renderToStaticMarkup(<HelloWorld data={dukpy.data}/>, null);
+'''
+        jsx = dukpy.jsx_compile(code)
+        res = dukpy.evaljs(jsx, data={'id': 1, 'name': "Alessandro"})
+        assert res == '<div class="helloworld">Hello Alessandro</div>', res
