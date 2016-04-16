7cd448c68ad43772212078b5b01a0256aa3d6b3c
Alessandro Molina <alessandro.molina@axant.it>
2016-04-16 15:25:12 +0200

    Test install command itself

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
 import shutil
 import tempfile
 
+import sys
+import mock
 import dukpy
+from dukpy import install as dukpy_install
 
 
 class TestPackageInstaller(object):
@@ -23,6 +27,11 @@ class TestPackageInstaller(object):
         jsi.loader.register_path(self.tmpdir)
         res = jsi.evaljs(jsx, data={'id': 1, 'name': "Alessandro"})
         assert res == '<div class="helloworld">Hello Alessandro</div>', res
+
+    def test_install_command(self):
+        with mock.patch.object(sys, 'argv', ['dukpy-install', 'react', '0.14.8']):
+            dukpy_install.main()
+        assert os.path.exists(os.path.join('./js_modules', 'react'))
 
 
 TEST_CODE = '''
