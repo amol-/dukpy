611dccc697f422ade188077dc69bec3daef4a27c
Alessandro Molina <alessandro.molina@axant.it>
2016-09-25 10:27:41 +0200

    Support for UMD modules

--- dukpy/webassets/babelfilter.py
+++ dukpy/webassets/babelfilter.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
+
+import os
+
 from webassets.filter import Filter
 
 import dukpy
@@ -16,5 +19,10 @@ class BabelJS(Filter):
     }
 
     def input(self, _in, out, **kw):
-        src = dukpy.babel_compile(_in.read())
+        options = {'filename': os.path.basename(kw['source_path'])}
+        if self.loader == 'systemjs':
+            options['plugins'] = ['transform-es2015-modules-systemjs']
+        elif self.loader == 'umd':
+            options['plugins'] = ['transform-es2015-modules-umd']
+        src = dukpy.babel_compile(_in.read(), **options)
         out.write(src['code'])
