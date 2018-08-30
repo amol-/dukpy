cc67d189cacb3c75922ce5d849554fac52e179ba
Alessandro Molina <alessandro.molina@axant.it>
2018-08-30 10:51:16 +0200

    State compatibility with Python 3.7

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.4',
