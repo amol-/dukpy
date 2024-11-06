f63e2c2168b390a7723dfb20562418f3c71fc963
Alessandro Molina <alessandro@molina.fyi>
2024-11-06 21:34:13 +0000

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ repo_url = 'https://github.com/{0}'.format(repo_slug)
 
 setup(
     name=name,
-    version='0.4.0',
+    version='0.5.0',
     description='Simple JavaScript interpreter for Python',
     long_description=README,
     keywords='javascript compiler babeljs jsx coffeescript typescript',
