ca8a3afac43db556cb01a6d6967b860464903b51
Alessandro Molina <alessandro@molina.fyi>
2025-12-22 21:58:00 +0100

    Remove Python 3.7 support reference

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: JavaScript',
     ],
     entry_points={
