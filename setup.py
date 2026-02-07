fcd2cf1f6437a9e043d9811ee7bf6d682cefaefa
Alessandro Molina <alessandro@molina.fyi>
2026-02-07 15:27:18 +0000

    Fix build on Windows

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 import os
+import sys
 from setuptools import Extension, setup
+
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c11"]
+    define_macros = []
+elif sys.platform.startswith("linux"):
+    extra_compile_args = ["-std=c11"]
+    define_macros = [("_GNU_SOURCE", "1")]
+else:
+    extra_compile_args = ["-std=c11"]
+    define_macros = []
 
 setup(
     ext_modules=[
@@ -14,6 +25,8 @@ setup(
             include_dirs=[
                 os.path.join(".", "src", "quickjs"),
             ],
+            extra_compile_args=extra_compile_args,
+            define_macros=define_macros,
         )
     ],
 )
